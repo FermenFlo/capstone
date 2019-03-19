0e28ea274a664723fb067cdb6fdaa081959d273f
Brian Joseph <joseph.b@husky.neu.edu>
2019-03-19 17:19:21 -0400

    restructured code

--- main.py
+++ main.py
@@ -1,30 +1,170 @@
+from collections import deque
+from imutils.video import VideoStream
+import numpy as np
+import argparse
 import cv2
-from datetime import datetime
-from constants import SLICE_INDICES
+import imutils
+import time
+import urllib.request
+import math 
 
-video = cv2.VideoCapture()
-# curl -o stream2.avi http://155.33.224.29:8080/4/video.cgi
 
-url = 'http://155.33.224.29:8080/4/video.cgi'
-video.open('stream')
-kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
-fgbg = cv2.createBackgroundSubtractorMOG2()
+class Stream:
+    def __init__(self, url = 'http://149.43.156.105/mjpg/video.mjpg', alterations = []):
+        self.url = url
+        self.alterations = ['crop'] + alterations # ORDER IS ORDER OF EXECUTION
+        self.stream = urllib.request.urlopen('http://149.43.156.105/mjpg/video.mjpg')
+        self.stream_bytes = bytes()
+        self.first_frame = None
+        self.contours = []
+        
+    def _grab_next_frame(self):
+        while True:
+            self.stream_bytes += self.stream.read(16)
+            a = self.stream_bytes.find(b'\xff\xd8')
+            b = self.stream_bytes.find(b'\xff\xd9')
 
-while(video.isOpened()):
+            if a != -1 and b != -1:
+                break
+
+        jpg = self.stream_bytes[a:b + 2]
+        self.stream_bytes = self.stream_bytes[b + 2:]
+        frame = cv2.imdecode(np.fromstring(jpg, dtype = np.uint8), cv2.IMREAD_COLOR)
+
+        if self.first_frame is None:
+            self.first_frame = frame
+            
+            for alteration in self.alterations:
+                if alteration != 'diff':
+                    self.first_frame = self._alter_frame(self.first_frame, alteration)
+                    
+        return frame
+        
+        
+    def _alter_frame(self, frame, alteration, kwargs = {}):
+        '''Given a frame and an alteration (e.g. "gray"), apply the alteation 
+           to the frame and return the new frame.
+           Also accepts kwargs for the alteration fucntion if needed
+           
+           implemented alterations:
+
+           - "crop": Crop the frame
+           - "gray": Gray the frame
+           - "blur": Blur the frame
+           - "dilate": Dilate the frame
+           - "erode": Erode the frame
+           - "diff": Take the difference between the frame and self.first_frame
+           - "thresh": Threshold the image above a certain value
+           - "hvs": Not entirely sure tbh...
+           '''
+        
+        if alteration == 'crop':
+            return imutils.resize(frame[250:500, 250:550], width = 600)
+        
+        elif alteration == 'gray':
+            return cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        
+        elif alteration == 'blur':
+            return cv2.GaussianBlur(frame, (11, 11), 0)
+        
+        elif alteration == 'dilate':
+            dilation_kernel = np.ones((5,1)) # use a large dilation kernel to capture sparse words
+            return cv2.dilate(frame, dilation_kernel, iterations = 1) # dilate
+        
+        elif alteration == 'erode':
+            erosion_kernel = np.ones((5,5)) # use a large dilation kernel to capture sparse words
+            return cv2.erode(frame, erosion_kernel, iterations = 1) # dilate
+        
+        elif alteration == 'diff':
+            if self.first_frame is None:
+                return frame
+            else:
+                return cv2.absdiff(frame, self.first_frame)
+        
+        elif alteration == 'thresh':
+            return cv2.threshold(frame, 25, 255, cv2.THRESH_BINARY)[1]
+        
+        elif alteration == 'hsv':
+            return cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        
+        
+    def _update_contours(self, frame, frame_number):
+        new_contours = cv2.findContours(self.current_frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        new_contours = imutils.grab_contours(new_contours)
+        print(self.first_frame)
+        
+#         for c in new_contours:
+#             if self.is_new_contour(c):
+#                 continue
+                
+#             else:
+#                 pass
+        pass
     
-    ret, frame = video.read()
+    def read_stream(self):
+        frame_number = 0
         
-    if ret:
-        fgmask = fgbg.apply(frame)
-        fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
-        cropped_frame = frame[:SLICE_INDICES[0], SLICE_INDICES[1]:]
+        while True:
+            frame = self._grab_next_frame()
+            
 
+            for alteration in self.alterations:
+                print(alteration)
+                frame = self._alter_frame(frame, alteration)
+
+
+            self.current_frame = frame
+            self._update_contours(frame, frame_number)
+            
+
+            cv2.imshow("Frame", self._alter_frame(self.current_frame, 'diff'))
+            
+            # if the 'q' key is pressed, stop the loop
+            key = cv2.waitKey(1) & 0xFF
+            frame_number += 1
+            if key == ord("q"):
+                break
+                
+                
+class Contour:
+    
+    def __init__(self, c, frame_number):
+        self.contour = c
+        self.frame_number = frame_number
+        self.age = 0
+        self.x, self.y, self.w, self.h = cv2.boundingRect(c)
+        self.moment = cv2.moments(c)
+        self.center = (int(self.moment["m10"] / self.moment["m00"]),
+                       int(self.moment["m01"] / self.moment["m00"]))
+        self.TL = (self.x, self.y)
+        self.TR = (self.x, self.y + self.w)
+        self.BL = (self.x + self.h, self.y)
+        self.BR = (self.x + self.h, self.y + self.w)
+        self.size = cv2.contourArea(c)
+        self.points = [center]
         
-        cv2.imshow('Marino', cropped_frame)
-   
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        
+    def get_closest_neighbor(self, contours):
+        dist = np.inf
+        neightbor = None
+        for c in contours:
+            c_dist = math.hypot(self.center[0] - c.center[0], self.center[1] - c.center[1])
+            
+            if c_dist <= dist:
+                dist, neighbor = c_dist, c
+                
+        self.neighbor_dist = dist
+        self.neighbor = c
+            
+    
+    def merge_contours(self, contour):
+        '''Given another contour, merge them together sensibly
+           and return the newly formed contour'''
+        pass
+        
 
-# release the file pointers
-video.release()
-cv2.destroyAllWindows()
+            
+if __name__ == '__main__':
+    
+    stream = Stream(alterations = ['gray', 'diff'])
+    stream.read_stream()
--- traffic_tracking.py
+++ traffic_tracking.py
@@ -15,9 +15,9 @@ import urllib.request
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video",
-	help="path to the (optional) video file")
+    help="path to the (optional) video file")
 ap.add_argument("-b", "--buffer", type=int, default=32,
-	help="max buffer size")
+    help="max buffer size")
 args = vars(ap.parse_args())
 
 delta_list = []
@@ -39,142 +39,142 @@ direction = ""
 # if a video path was not supplied, grab the reference
 # to the webcam
 if not args.get("video", False):
-	vs = VideoStream(src=0).start()
+    vs = VideoStream(src=0).start()
 
 # otherwise, grab a reference to the video file
 else:
-	firstFrame = None
-	stream = urllib.request.urlopen('http://149.43.156.105/mjpg/video.mjpg')
-	bytes = bytes()
+    firstFrame = None
+    stream = urllib.request.urlopen('http://149.43.156.105/mjpg/video.mjpg')
+    stream_bytes = bytes()
 # allow the camera or video file to warm up
 time.sleep(2.0)
 
 # keep looping
 while True:
-	bytes += stream.read(16)
-	a = bytes.find(b'\xff\xd8')
-	b = bytes.find(b'\xff\xd9')
-	if a != -1 and b != -1:
-		jpg = bytes[a:b + 2]
-		bytes = bytes[b + 2:]
-		frame = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
-		frame = frame[250:500, 250:550]
-		frame = imutils.resize(frame, width=600)
-		blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-		hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+    stream_bytes += stream.read(16)
+    a = stream_bytes.find(b'\xff\xd8')
+    b = stream_bytes.find(b'\xff\xd9')
+    if a != -1 and b != -1:
+        jpg = stream_bytes[a:b + 2]
+        stream_bytes = stream_bytes[b + 2:]
+        frame = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
+        frame = frame[250:500, 250:550]
+        frame = imutils.resize(frame, width=600)
+        blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+        hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
-		#frame = cv2.fastNlMeansDenoisingColored(frame, None, 10, 10, 3, 21)
-		# resize the frame, convert it to grayscale, and blur it
-		frame = imutils.resize(frame, width=500)
-		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-		gray = cv2.GaussianBlur(gray, (21, 21), 0)
+        #frame = cv2.fastNlMeansDenoisingColored(frame, None, 10, 10, 3, 21)
+        # resize the frame, convert it to grayscale, and blur it
+        frame = imutils.resize(frame, width=500)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
-		if firstFrame is None:
-			firstFrame = gray
-			continue
+        if firstFrame is None:
+            firstFrame = gray
+            continue
 
-		center = None
+        center = None
 
-		# compute the absolute difference between the current frame and
-		# first frame
-		frameDelta = cv2.absdiff(firstFrame, gray)
-		thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+        # compute the absolute difference between the current frame and
+        # first frame
+        frameDelta = cv2.absdiff(firstFrame, gray)
+        thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
 
-		# dilate the thresholded image to fill in holes, then find contours
-		# on thresholded image
-		thresh = cv2.dilate(thresh, None, iterations=2)
-		cnts = cv2.findContours(thresh.copy(), cv2.RETR_TREE,
-								cv2.CHAIN_APPROX_NONE)
-		cnts = imutils.grab_contours(cnts)
+        # dilate the thresholded image to fill in holes, then find contours
+        # on thresholded image
+        thresh = cv2.dilate(thresh, None, iterations=2)
+        dilation_kernel = np.ones((5,1)) # use a large dilation kernel to capture sparse words
+        dilated = cv2.dilate(thresh, dilation_kernel, iterations = 2) # dilate
+        
+        cnts = cv2.findContours(dilated.copy(), cv2.RETR_TREE,
+                                cv2.CHAIN_APPROX_NONE)
+        cnts = imutils.grab_contours(cnts)
 
 
-		# only proceed if at least one contour was found
-		if len(cnts) > 0:
-			# find the largest contour in the mask, then useq
-			# it to compute the minimum enclosing rectangle and centroid
-			cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
-			cnts = cnts[0:10]
-			for c in cnts:
-				(x, y, w, h) = cv2.boundingRect(c)
-				#contour_valid = (w >= min_contour_width and w <= max_contour_width) and (h >= min_contour_height and h<= max_contour_height)
-				#contour_valid = (h > w) and (w < max_contour_width)
-				contour_valid = ((cv2.contourArea(c)>500))
+        # only proceed if at least one contour was found
+        if len(cnts) > 0:
+            # find the largest contour in the mask, then useq
+            # it to compute the minimum enclosing rectangle and centroid
+            cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+            cnts = cnts[0:10]
+            for c in cnts:
+                (x, y, w, h) = cv2.boundingRect(c)
 
-				if not contour_valid:
-					continue
+                if cv2.contourArea(c) <= 500:
+                    continue
 
-				M = cv2.moments(c)
-				center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+                M = cv2.moments(c)
+                center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
-				# only proceed if the radius meets a minimum size
-				# draw the rectangle and centroid on the frame,
-				# then update the list of tracked points
-				cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-				pts.appendleft(center)
+                # only proceed if the radius meets a minimum size
+                # draw the rectangle and centroid on the frame,
+                # then update the list of tracked points
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                pts.appendleft(center)
 
-		# loop over the set of tracked points
-		for i in np.arange(1, len(pts)):
-			# if either of the tracked points are None, ignore them
-			if pts[i - 1] is None or pts[i] is None:
-				continue
+        # loop over the set of tracked points
+        for i in np.arange(1, len(pts)):
+            # if either of the tracked points are None, ignore them
+            if pts[i - 1] is None or pts[i] is None:
+                continue
 
-			# check to see if enough points have been accumulated in
-			# the buffer
-			if counter >= 10 and i == 1 and pts[-1] is not None:
-				# compute the difference between the x and y
-				# coordinates and re-initialize the direction
-				# text variables
-				if center:
-					dX = pts[-1][0] - pts[i][0]
-					dY = pts[-1][1] - pts[i][1]
-				else:
-					dX = 0
-					dY = 0
-				(dirX, dirY) = ("", "")
+            # check to see if enough points have been accumulated in
+            # the buffer
+            if counter >= 10 and i == 1 and pts[-1] is not None:
+                # compute the difference between the x and y
+                # coordinates and re-initialize the direction
+                # text variables
+                if center:
+                    dX = pts[-1][0] - pts[i][0]
+                    dY = pts[-1][1] - pts[i][1]
+                else:
+                    dX = 0
+                    dY = 0
+                (dirX, dirY) = ("", "")
 
-				# ensure there is significant movement in the
-				# x-direction
-				if np.abs(dX) > 20:
-					dirX = "East" if np.sign(dX) == 1 else "West"
+                # ensure there is significant movement in the
+                # x-direction
+                if np.abs(dX) > 20:
+                    dirX = "East" if np.sign(dX) == 1 else "West"
 
-				# ensure there is significant movement in the
-				# y-direction
-				if np.abs(dY) > 20:
-					dirY = "North" if np.sign(dY) == 1 else "South"
+                # ensure there is significant movement in the
+                # y-direction
+                if np.abs(dY) > 20:
+                    dirY = "North" if np.sign(dY) == 1 else "South"
 
-				# handle when both directions are non-empty
-				if dirX != "" and dirY != "":
-					direction = "{}-{}".format(dirY, dirX)
+                # handle when both directions are non-empty
+                if dirX != "" and dirY != "":
+                    direction = "{}-{}".format(dirY, dirX)
 
-				# otherwise, only one direction is non-empty
-				else:
-					direction = dirX if dirX != "" else dirY
+                # otherwise, only one direction is non-empty
+                else:
+                    direction = dirX if dirX != "" else dirY
 
-			# otherwise, compute the thickness of the line and
-			# draw the connecting lines
-			thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-			cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+            # otherwise, compute the thickness of the line and
+            # draw the connecting lines
+            thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+            cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
 
-		# show the movement deltas and the direction of movement on
-		# the frame
-		#print("dx: {}, dy: {}".format(dX, dY))
-		print(dX,dY)
-		delta_list.append((dX,dY))
-		cv2.putText(frame, direction, (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
-			0.65, (0, 0, 255), 3)
-		cv2.putText(frame, "dx: {}, dy: {}".format(dX, dY),
-			(10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
-			0.35, (0, 0, 255), 1)
+        # show the movement deltas and the direction of movement on
+        # the frame
+        #print("dx: {}, dy: {}".format(dX, dY))
+        print(dX,dY)
+        delta_list.append((dX,dY))
+        cv2.putText(frame, direction, (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
+            0.65, (0, 0, 255), 3)
+        cv2.putText(frame, "dx: {}, dy: {}".format(dX, dY),
+            (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+            0.35, (0, 0, 255), 1)
 
-		# show the frame to our screen and increment the frame counter
-		cv2.imshow("Frame", frame)
-		#cv2.imshow("Thresh", thresh)
-		key = cv2.waitKey(1) & 0xFF
-		counter += 1
+        # show the frame to our screen and increment the frame counter
+        cv2.imshow("Frame", frame)
+        cv2.imshow("Dilation", dilated)
+        key = cv2.waitKey(1) & 0xFF
+        counter += 1
 
-		# if the 'q' key is pressed, stop the loop
-		if key == ord("q"):
-			break
+        # if the 'q' key is pressed, stop the loop
+        if key == ord("q"):
+            break
 
 
 
@@ -186,7 +186,7 @@ print(w_list)
 
 # if we are not using a video file, stop the camera video stream
 if not args.get("video", False):
-	vs.stop()
+    vs.stop()
 
 # otherwise, release the camera
 #else:
