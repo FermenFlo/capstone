c19426103b8a4ce7472c708029e642f7b6c275c4
Brian Joseph <joseph.b@husky.neu.edu>
2019-03-24 19:46:29 -0400

    contours showing up

--- main.py
+++ main.py
@@ -10,12 +10,13 @@ import math
 
 
 class Stream:
-    def __init__(self, url = 'http://149.43.156.105/mjpg/video.mjpg', alterations = []):
+    def __init__(self, url = 'http://149.43.156.105/mjpg/video.mjpg', base_alterations = ['crop', 'gray', 'blur']):
         self.url = url
-        self.alterations = ['crop'] + alterations # ORDER IS ORDER OF EXECUTION
+        self.base_alterations = base_alterations # ORDER IS ORDER OF EXECUTION
         self.stream = urllib.request.urlopen('http://149.43.156.105/mjpg/video.mjpg')
         self.stream_bytes = bytes()
         self.first_frame = None
+        self.frame_number = 0
         self.contours = []
         
     def _grab_next_frame(self):
@@ -32,11 +33,8 @@ class Stream:
         frame = cv2.imdecode(np.fromstring(jpg, dtype = np.uint8), cv2.IMREAD_COLOR)
 
         if self.first_frame is None:
-            self.first_frame = frame
-            
-            for alteration in self.alterations:
-                if alteration != 'diff':
-                    self.first_frame = self._alter_frame(self.first_frame, alteration)
+            for alteration in self.base_alterations:
+                self.first_frame = self._alter_frame(frame, alteration)
                     
         return frame
         
@@ -68,63 +66,65 @@ class Stream:
             return cv2.GaussianBlur(frame, (11, 11), 0)
         
         elif alteration == 'dilate':
-            dilation_kernel = np.ones((5,1)) # use a large dilation kernel to capture sparse words
+            dilation_kernel = np.ones((5,1)) # use a large dilation kernel
             return cv2.dilate(frame, dilation_kernel, iterations = 1) # dilate
         
         elif alteration == 'erode':
-            erosion_kernel = np.ones((5,5)) # use a large dilation kernel to capture sparse words
+            erosion_kernel = np.ones((5,5)) # use a large dilation kernel
             return cv2.erode(frame, erosion_kernel, iterations = 1) # dilate
         
         elif alteration == 'diff':
-            if self.first_frame is None:
-                return frame
-            else:
+            try: # Doesn't work if on first frame
                 return cv2.absdiff(frame, self.first_frame)
+            
+            except:
+                return frame
         
         elif alteration == 'thresh':
-            return cv2.threshold(frame, 25, 255, cv2.THRESH_BINARY)[1]
+            return cv2.threshold(frame, 125, 255, cv2.THRESH_BINARY)[1]
         
         elif alteration == 'hsv':
             return cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         
         
-    def _update_contours(self, frame, frame_number):
-        new_contours = cv2.findContours(self.current_frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    def _update_contours(self):
+        new_contours = cv2.findContours(self.altered_frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
         new_contours = imutils.grab_contours(new_contours)
-        print(self.first_frame)
         
-#         for c in new_contours:
-#             if self.is_new_contour(c):
-#                 continue
-                
-#             else:
-#                 pass
-        pass
-    
+        self.contours += [Contour(c, self.frame_number) for c in new_contours]
+        
+        
+    def draw_contours(self):
+        for c in self.contours:
+            cv2.rectangle(self.current_frame, c.TL, c.BR, (0, 0, 255), 2)
+        
     def read_stream(self):
-        frame_number = 0
-        
         while True:
-            frame = self._grab_next_frame()
+            self.current_frame = self._grab_next_frame()
             
-
-            for alteration in self.alterations:
-                print(alteration)
-                frame = self._alter_frame(frame, alteration)
-
-
-            self.current_frame = frame
-            self._update_contours(frame, frame_number)
+            for alteration in self.base_alterations:
+                self.current_frame = self._alter_frame(self.current_frame, alteration)
             
-
-            cv2.imshow("Frame", self._alter_frame(self.current_frame, 'diff'))
             
-            # if the 'q' key is pressed, stop the loop
+            self.altered_frame = self.current_frame.copy()
+            
+            for alteration in ['diff', 'thresh']:
+                self.altered_frame = self._alter_frame(self.altered_frame, alteration)
+
+
+            self._update_contours()
+            self.draw_contours()
+            cv2.imshow("Frame", self.current_frame)
+            cv2.imshow("First Frame", self.first_frame)
+            
+            # if the 'q' key is pressed, stop the loopa
             key = cv2.waitKey(1) & 0xFF
-            frame_number += 1
+            self.frame_number += 1
             if key == ord("q"):
                 break
                 
+                
+
 
 class Contour:
     
@@ -134,27 +134,26 @@ class Contour:
         self.age = 0
         self.x, self.y, self.w, self.h = cv2.boundingRect(c)
         self.moment = cv2.moments(c)
-        self.center = (int(self.moment["m10"] / self.moment["m00"]),
-                       int(self.moment["m01"] / self.moment["m00"]))
+        self.center = (int(self.moment["m10"] / (self.moment["m00"] or 1)),
+                       int(self.moment["m01"] / (self.moment["m00"] or 1)))
         self.TL = (self.x, self.y)
         self.TR = (self.x, self.y + self.w)
         self.BL = (self.x + self.h, self.y)
         self.BR = (self.x + self.h, self.y + self.w)
         self.size = cv2.contourArea(c)
-        self.points = [center]
+        self.points = [self.center]
         
         
     def get_closest_neighbor(self, contours):
         dist = np.inf
         neightbor = None
+        
         for c in contours:
             c_dist = math.hypot(self.center[0] - c.center[0], self.center[1] - c.center[1])
             
             if c_dist <= dist:
-                dist, neighbor = c_dist, c
-                
-        self.neighbor_dist = dist
-        self.neighbor = c
+                self.neighbor_dist , self.neighbor = c_dist, c
+
             
 
 # get closest neighbor via time and distance
@@ -164,20 +163,18 @@ class Contour:
         '''Given another contour, merge them together sensibly
            and return the newly formed contour'''
         c_dist = math.hypot(self.center[0] - contour.center[0], self.center[1] - contour.center[1])
-        too_close_dist = 5
+        too_close_dist = 0
         if c_dist < too_close_dist:
-            #c = cv2.convexHull(self.contour)
             TL = (min(self.TL[0], contour.TL[0]), min(self.TL[1], contour.TL[1]))
             TR = (max(self.TL[0], contour.TL[0]), min(self.TL[1], contour.TL[1]))
             BL = (min(self.TL[0], contour.TL[0]), max(self.TL[1], contour.TL[1]))
             BR = (max(self.TL[0], contour.TL[0]), max(self.TL[1], contour.TL[1]))
-            #cv2.rectangle(frame, TL, BR)
 
 
             
 if __name__ == '__main__':
     
-    stream = Stream(alterations = ['gray', 'diff'])
+    stream = Stream()
     stream.read_stream()
 
 
